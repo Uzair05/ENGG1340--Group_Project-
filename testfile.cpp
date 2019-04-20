bf5d7a0a67abb2fbec60a90c6562f27f35133959
Uzair Bin Colangoy Asim <u3560307@connect.hku.hk>
2019-04-20 20:09:11 +0800

    Uzair, If i die will the assignments end?

--- testfile.cpp
+++ testfile.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
 #include <string>
-#include "RestaurantTables.h" /*the double quotation marks makes it serach for header
-from within the same directory*/
+#include "RestaurantTables.h"
 #include "TableFunctions.h"
+
+#include "TableHandle.h"
+
 using namespace std;
 
 
 int main(){
-  RTables FDeli[20];
 
+  RTables* a = new RTables[getsize()];
+  LoadTables(a);
 
-  /*temporary expo*/
-  string id="stove";
-  int NumTables;
-  bool isOccupied;
+  cout<<a[2].getID()<<endl;
 
-  /*set up*/
-
-  for(int i=0;i<20;++i){
-    FDeli[i].setID(id);
-    FDeli[i].setNofSeats(i);
-    FDeli[i].setOccupied(false);
-  }
-
-  ReleaseTable(FDeli,"stove",20);
-  cout<<FDeli[13].getID()<<" "<<FDeli[13].getNofSeats()<<endl;
-
-
-
-  /*temporary expo*/
-
-
+  delete[] a;
   return 0;
 }
