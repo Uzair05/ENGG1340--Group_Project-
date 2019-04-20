1ab78848f961e1c9abab431586e855934f138854
Uzair Bin Colangoy Asim <u3560307@connect.hku.hk>
2019-04-20 13:12:09 +0800

    more changes

--- alert_aid.h
+++ alert_aid.h
@@ -1,6 +1,9 @@
 #ifndef Alert_AID_H
 #define Alert_AID_H
 
+/*I made this to hadle all our alerts without
+needing to continuously refer to the proper format*/
+
 #include <string>
 #include <iostream>
 #include <windows.h>
