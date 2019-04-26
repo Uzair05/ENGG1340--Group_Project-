1ba34ebd597255d3c1f03aa76e971b950a896123
tridib13 <47222427+tridib13@users.noreply.github.com>
2019-04-26 15:35:18 +0800

    Add files via upload

---
+++ RestaurantTables.h
@@ -0,0 +1,70 @@
+#ifndef RTables_H
+#define RTables_H
+
+#include <string>
+#include "TableTop.h"
+
+using namespace std;
+
+class RTables{
+private:
+  table tab;
+public:
+
+  string getID(){
+    return tab.ID;
+  }
+
+  int getNofSeats(){
+    return tab.NumberOfSeats;
+  }
+
+  bool getIsOccupied(){
+    return tab.Occupied;
+  }
+
+  string getBookingTime(){
+    return tab.BookingTime;
+  }
+
+  string getSittingTime(){
+    return tab.SittingTime;
+  }
+
+  string getCustomerName(){
+    return tab.CustomerName;
+  }
+
+
+  void setID(string s){
+    tab.ID=s;
+  }
+
+  void setNofSeats(int n){
+    tab.NumberOfSeats=n;
+  }
+
+  void toggleOccupied(){
+    tab.Occupied=(!(tab.Occupied));
+  }
+
+  void setOccupied(bool b){
+    tab.Occupied=b;
+  }
+
+  void setBookingTime(string booking){
+    tab.BookingTime=booking;
+  }
+
+  void setSittingTime(string sitting){
+    tab.SittingTime=sitting;
+  }
+
+  void setCustomerName(string customer){
+    tab.CustomerName=customer;
+  }
+
+
+};
+
+#endif
---
+++ TableTop.h
@@ -0,0 +1,21 @@
+#ifndef TableTop_H
+#define TableTop_H
+
+#include <string>
+
+struct table{
+
+std::string ID;
+int NumberOfSeats;
+bool Occupied;
+string BookingTime;
+string SittingTime;
+string CustomerName;
+=======
+/*Date Time Seated*/
+/*Date Time Booking*/
+
+>>>>>>> cd1f208c52d832e9c80e780c33d3a97f1924f4e6
+};
+
+#endif
