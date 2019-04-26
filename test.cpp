3de53da4fd1c4603378b67ca9f250bfddd2ccc1a
Uzair Bin Colangoy Asim <u3560307@connect.hku.hk>
2019-04-26 22:09:30 +0800

    Completed another function of the Reservation

--- test.cpp
+++ test.cpp
@@ -5,22 +5,62 @@
 using namespace std;
 
 
-void SetBooking(string ID,string tim){
-  ofstream foi;
-  foi.open("Bookings.txt",ios::app);
-  if (foi.fail()){
+void CancelBooking(string ID,string tim){
+  ifstream fin;
+  string temp;
+  string temp2;
+  string identity,clock;
+
+  ofstream fout;
+  fout.open("Temp.txt");
+  if (fout.fail()){
+    system("clear");
+    cout<<"\aUnable to access Temp.txt"<<endl;
+    exit(1);
+  }
+
+  fin.open("Bookings.txt");
+  if (fin.fail()){
     system("clear");
     cout<<"\aUnable to access Bookings.txt"<<endl;
     exit(1);
   }else{
-    foi<<ID<<" "<<tim<<endl;
-    foi.close();
+
+    while(getline(fin,temp)){
+      istringstream iss(temp);
+      iss>>identity>>clock;
+      if (!((identity==ID)&&(clock==tim))){
+        fout<<identity<<" "<<clock<<endl;
+        /*transfers everything except item to be deleted*/
+      }
+    }
+    fout.close();
+    fin.close();
+
+    ifstream foilin;
+    ofstream foilout;
+    foilin.open("Temp.txt");
+    if (foilin.fail()){
+      system("clear");
+      cout<<"\aUnable to access Temp.txt"<<endl;
+      exit(1);
+    }
+    foilout.open("Bookings.txt");
+    if (foilout.fail()){
+      system("clear");
+      cout<<"\aUnable to access Bookings.txt"<<endl;
+      exit(1);
+    }
+    while(getline(foilin,temp)){
+      foilout<<temp<<endl;
+      /*transfers everything into original file.*/
+    }
   }
 }
 
 int main(){
 
-SetBooking("Uzair","13:20");
+CancelBooking("Umair","13:00");
 
 return 0;
 }
