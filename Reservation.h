5310a80f2a2ffff7ef358fe860755113aafccbf1
Uzair Bin Colangoy Asim <u3560307@connect.hku.hk>
2019-04-27 23:41:03 +0800

    foil repair

--- Reservation.h
+++ Reservation.h
@@ -32,7 +32,7 @@ void CheckOverTime(string tim);
 
 /*Accepts a manual input of time in 24 hours format*/
 
-void FindTime(string ID,string tim);
+bool FindTime(string ID,string tim);
 /*Accepts ID and time of booking and checks if it is possible within that time space*/
 /*only sends true if it doesnt overlap 45 minuts with other bookings*/
 
